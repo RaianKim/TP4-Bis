c6076f03640a7d62ccc7c75190066f508f1ec750
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-05-18 10:57:32 -0300

    Comenzamos con el trabajo

--- Models/Info.cs
+++ Models/Info.cs
@@ -6,7 +6,7 @@ private static List<Pais> ListaPaises = new List<Pais>();
 private static void IniciarLista()
 {
     
-    ListaPaises.Add(new Pais{Nombre = "Argentina", });
+    ListaPaises.Add(new Pais("Argentina","/Imagenes/BanderaArgentina.png",45810000,new DateTime(09/07/1816),"La Boca es un mítico barrio que se encuentra en la ciudad de Buenos Aires donde se mantiene una estética muy particular que te remonta a los primeros años cuando los inmigrantes llegaron al país."));
     
     
 }
--- Models/Pais.cs
+++ Models/Pais.cs
@@ -1,8 +1,18 @@
-class Pais
+public class Pais
 {
 public string Nombre {get; private set;}
 public string imgBandera {get; private set;}
 public int Poblacion {get; private set;}
 public DateTime FechaIndependencia{get; private set;}
 public string AtractivosTuristicos {get; private set;}
+
+public Pais()
+{
+
 }
+public Pais(string nom, string img, int pob, DateTime fecha, string AtracTuristicos)
+{
+
+}
+}
+
